5204588f299b2e2eedcdb7cae586abc9784f65f8
Kenneth Hoff <kenneth.hoff@oxx.no>
2026-02-06 19:38:50 +0100

    build(nix): add bun

--- flake.nix
+++ flake.nix
@@ -9,7 +9,9 @@
       systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
       perSystem = {pkgs, ...}: {
         devShells.default = pkgs.mkShell {
-          packages = [];
+          packages = [
+            pkgs.bun
+          ];
         };
       };
     };
