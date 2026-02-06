de46e37d9966da16c7fef3e37d5aedbfdded7641
Kenneth Hoff <kenneth.hoff@oxx.no>
2026-02-06 22:34:41 +0100

    feat(webhooks): add some console logging

--- applications/spotify_player/src/webhooks/spotify-track-id.ts
+++ applications/spotify_player/src/webhooks/spotify-track-id.ts
@@ -1,10 +1,12 @@
 export const hook = async (req: Request): Promise<Response> => {
 
         if (!req.body) {
+                console.log("No body");
                 return new Response("No body");
         }
 
         const json = await req.body.json();
-
+        
+        console.log(json);
         return new Response(json);
 };
