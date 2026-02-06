86409e940917edd6fc6ea214731d29d0bcdd10d6
Kenneth Hoff <kenneth.hoff@oxx.no>
2026-02-06 20:26:39 +0100

    feat(spotify_player): remove api endpoints

--- applications/spotify_player/src/the-app.ts
+++ applications/spotify_player/src/the-app.ts
@@ -12,25 +12,6 @@ const server = serve({
     "/": homepage,
     // Bundle & route dashboard.html to "/dashboard"
     "/dashboard": dashboard,
-
-    // ** API endpoints ** (Bun v1.2.3+ required)
-    "/api/users": {
-      async GET(req) {
-        const users = await sql`SELECT * FROM users`;
-        return Response.json(users);
-      },
-      async POST(req) {
-        const { name, email } = await req.json();
-        const [user] =
-          await sql`INSERT INTO users (name, email) VALUES (${name}, ${email})`;
-        return Response.json(user);
-      },
-    },
-    "/api/users/:id": async (req) => {
-      const { id } = req.params;
-      const [user] = await sql`SELECT * FROM users WHERE id = ${id}`;
-      return Response.json(user);
-    },
   },
 
   // Enable development mode for:
