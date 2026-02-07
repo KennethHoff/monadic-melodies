76b354ccf1d254afa39079a39562dcc086e3a8a5
Kenneth Hoff <kenneth.hoff@oxx.no>
2026-02-07 01:20:28 +0100

    feat(spotify_player): fetch current track uri from the api

--- applications/spotify_player/src/spotify-embed.ts
+++ applications/spotify_player/src/spotify-embed.ts
@@ -1,7 +1,9 @@
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
+window.onSpotifyIframeApiReady = async (IFrameAPI) => {
+  const result = await fetch("/api/songs/current");
+  const json = await result.json();
   const element = document.querySelector('#embed-iframe');
   const options = {
-      uri: 'spotify:track:5NEKjqTQPKiqOiOG8YxLdS'
+      uri: json
     };
   const callback = (EmbedController) => {};
   IFrameAPI.createController(element, options, callback);
